b03828ab761d1bc089c0111495f4ab62663c2407
endymionsf <endymionsf>
2003-04-29 19:46:56 +0000

    wonrking on event ONHEAR

--- amx/amxwrap2.cpp
+++ amx/amxwrap2.cpp
@@ -944,7 +944,7 @@ NATIVE2(_setCharProperty)
 			break;
 		case NXW_CP_STR_SPEECH_CURRENT :			 		//dec value: 459;
 			pc->speechCurrent= new cUnicodeString( std::string( g_cAmxPrintBuffer ) );
-			break;
+			break; 
 		default :
 			ErrOut("chr_setProperty called with invalid property %d!\n", params[2] );
 			break;
