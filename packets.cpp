ec6e0b6c43664ca76e321585a8ced5372ffe47e6
endymionsf <endymionsf>
2003-05-04 18:12:04 +0000

    little fix, my guilty

--- packets.cpp
+++ packets.cpp
@@ -374,6 +374,7 @@ CREATE( CharProfile, PKG_CHAR_PROFILE, 0x07 )
 SEND( CharProfile ) {
 	if( ps == NULL ) return; 
 	if( profile==NULL ) profile=&emptyUnicodeString;
+	if( staticProfile==NULL ) staticProfile=&emptyUnicodeString;
 	this->size=this->headerSize +(title.size()+1) + profile->size() + staticProfile->size();
 	Xsend( ps->toInt(), this->getBeginValid(), this->headerSize );
 	Xsend( ps->toInt(), this->title.c_str(), title.size()+1 );
