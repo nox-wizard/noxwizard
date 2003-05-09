2d8bf8c1f7f4e77577506cd3f78abd5a18ddcb07
endymionsf <endymionsf>
2003-05-09 22:33:36 +0000

    *** empty log message ***

--- menu.h
+++ menu.h
@@ -27,11 +27,11 @@ class cMenu
 {
 	private:
 		static MENU_MAP menuMap;
-		static map<UI32, std::string > responseMap;
+		static map< UI32, std::string > responseMap;
 		static LOGICAL	addResponse( UI32 fieldID, std::string value );
 	public:
-		static LOGICAL	createGump(UI32 gump, UI32 x, UI32 y, bool canMove, bool canClose, bool canDispose, UI32 serial);
-		static LOGICAL	createGump(UI32 gump, UI32 x, UI32 y, UI08 options, UI32 serial);
+		static LOGICAL	createGump( UI32 gump, UI32 x, UI32 y, bool canMove, bool canClose, bool canDispose, UI32 serial);
+		static LOGICAL	createGump( UI32 gump, UI32 x, UI32 y, UI08 options, UI32 serial);
 		static LOGICAL	deleteGump( UI32 gump );
 		static LOGICAL	handleGump( NXWCLIENT ps );
 		static cMenu* selectGump( UI32 gump );
