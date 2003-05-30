a302b406668dc0901b94f012977d626651afc4cd
endymionsf <endymionsf>
2003-05-30 22:49:25 +0000

    *** empty log message ***

--- typedefs.h
+++ typedefs.h
@@ -72,6 +72,7 @@ typedef SI32				ACCOUNT;
 typedef std::vector<UI32>		UI32VECTOR;	//!< vector of unsigned 32 bits integers
 typedef std::vector<SERIAL>		SERIAL_VECTOR;	//!< vector of serials
 typedef slist<SERIAL>                   SERIAL_SLIST;	//!< slist of serials
+typedef SERIAL SCRIPTID; //!< a script id
 
 typedef std::set<SERIAL>		SERIAL_SET;	//!< set of serials
 
