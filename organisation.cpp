11c87b8f08d3dc1afea64b835256b7c903d40a3a
sparhawksf <sparhawksf>
2003-06-09 18:28:39 +0000

    Preparation for a completely new guild implementation

--- organisation.cpp
+++ organisation.cpp
@@ -3,53 +3,269 @@
 
 namespace Organisation
 {
+	class Member
+	{
+		public:
+					Member();
+					Member( P_CHAR pChar );
+					~Member();
+		private:
+			SERIAL		serial;
+			cGameDate	dateJoined;
+		public:
+			SERIAL		getSerial();
+	};
+
+	Member::Member()
+	{
+		serial = INVALID;
+	}
+
+	Member::Member( P_CHAR pChar )
+	{
+		serial = pChar->getSerial32();
+	}
+
+	Member::~Member()
+	{
+	}
+
+	SERIAL Member::getSerial()
+	{
+		return serial;
+	}
+
+	typedef Member*			P_MEMBER;
+	typedef map< SERIAL, P_MEMBER > memberMap;
+	typedef memberMap::iterator	memberMapIterator;
+
+	class Organisation
+	{
+		public:
+					Organisation();
+					Organisation( P_ITEM pFocus );
+			virtual		~Organisation();
+		private:
+			SERIAL		focusSerial;
+			std::string	name;
+			Member*		leader;
+			memberMap	members;
+			cGameDate	dateCreated;
+		public:
+			LOGICAL		isMember( SERIAL who );
+			SI32		numberOfMembers();
+			virtual LOGICAL	addMember( P_MEMBER pWho );
+			virtual LOGICAL	setLeader( P_MEMBER pWho );
+			LOGICAL		isLeader( Member& who );
+			LOGICAL		isLeader( SERIAL who );
+	};
+
+	Organisation::Organisation()
+	{
+		focusSerial 	= INVALID;
+	}
+
+	Organisation::Organisation( P_ITEM pFocus )
+	{
+		focusSerial 	= pFocus->getSerial32();
+	}
+
+	Organisation::~Organisation()
+	{
+	}
+
+	LOGICAL Organisation::isMember( SERIAL who )
+	{
+		return members.find( who ) != members.end();
+	}
+
+	/*!
+	\brief	Add a member to an organisation
+	\author Sparhawk
+	\param	pWho pointer to member instance.
+	\return boolean true on success, otherwise false
+	\since	0.82a
+	*/
+	LOGICAL Organisation::addMember( P_MEMBER pWho )
+	{
+		SERIAL serial = pWho->getSerial();
+
+		if( !isMember( serial ) )
+		{
+			members[ serial ] = pWho;
+			return true;
+		}
+		else
+			return false;
+	}
+
+	/*!
+	\brief	Set the leader of an organisation
+	\note	A leader has to be a member of an organisation.
+	\author Sparhawk
+	\param	pWho pointer to member instance.
+	\return boolean true on success, otherwise false
+	\since	0.82a
+	*/
+	LOGICAL Organisation::setLeader( P_MEMBER pWho )
+	{
+		if( isMember( pWho->getSerial() ) )
+		{
+			leader = pWho;
+			return true;
+		}
+		else
+			return false;
+	}
+
+	/*!
+	\brief	Get the number of members in an organisation
+	\author Sparhawk
+	\return number of members
+	\since	0.82a
+	*/
+	SI32 Organisation::numberOfMembers()
+	{
+		return members.size();
+	}
+
+	LOGICAL	Organisation::isLeader( Member &who )
+	{
+		return isLeader( who.getSerial() );
+	}
+
+	LOGICAL	Organisation::isLeader( SERIAL who )
+	{
+		if( leader )
+			if( leader->getSerial() == who )
+				return true;
+		return false;
+	}
+}
+
+namespace Organisation
+{
 	namespace Guild
 	{
+		class GuildMember;
 
-		class Guild
+		class GuildMember : public Member
+		{
+			public:
+						GuildMember( P_CHAR pWho );
+				virtual		~GuildMember();
+			private:
+				GuildMember*	fealty;
+				UI32		loyaltyBase;
+		};
+
+		GuildMember::GuildMember( P_CHAR pWho ) : Member( pWho )
+		{
+			fealty		= this;
+			loyaltyBase	= 1;
+		}
+
+		GuildMember::~GuildMember()
+		{
+		}
+
+		class GuildRecruit : public Member
+		{
+			public:
+						GuildRecruit( P_CHAR pWho );
+				virtual		~GuildRecruit();
+		};
+
+		GuildRecruit::GuildRecruit( P_CHAR pWho ) : Member( pWho )
+		{
+		}
+
+		GuildRecruit::~GuildRecruit()
+		{
+		}
+
+		class Guild : public Organisation
 		{
 			public:
 						Guild( P_ITEM pStone, P_CHAR pCreator );
 						~Guild();
 			private:
-				SERIAL		guildStoneSerial;
-				SERIAL		creatorSerial;
-				cGameDate	dateCreated;
-				std::string	name;
-				std::string	abbreviation;
-				SERIAL		guildMasterSerial;
+				char		abbreviation[4];
+				memberMap	recruits;
+			public:
+				LOGICAL		isRecruit( SERIAL who );
+				SI32		numberOfRecruits();
+				LOGICAL		addRecruit( P_CHAR pWho );
+
+				virtual LOGICAL	addMember( P_MEMBER pWho );
 		};
 
-		Guild::Guild( P_ITEM pStone, P_CHAR pCreator )
+		Guild::Guild( P_ITEM pStone, P_CHAR pCreator ) : Organisation( pStone )
 		{
-			guildStoneSerial = pStone->getSerial32();
-			creatorSerial = pCreator->getSerial32();
-			guildMasterSerial = creatorSerial;
+			GuildMember *member = new GuildMember( pCreator );
+
+			Organisation::addMember( member );
+			setLeader( member );
 		}
 
 		Guild::~Guild()
 		{
 		}
 
-		typedef Guild* P_GUILD;
-		typedef map< SERIAL, P_GUILD > guildMap;
-		typedef guildMap::iterator guildMapIterator;
-		typedef map< SERIAL, SERIAL > guildMemberMap;
-		typedef guildMemberMap::iterator guildMemberMapIterator;
+		LOGICAL Guild::isRecruit( SERIAL who )
+		{
+			return recruits.find( who ) != recruits.end();
+		}
+
+		SI32 Guild::numberOfRecruits()
+		{
+			return recruits.size();
+		}
+
+		LOGICAL Guild::addRecruit( P_CHAR pWho )
+		{
+			LOGICAL added = false;
+
+			if( ISVALIDPC( pWho ) )
+			{
+				SERIAL who = pWho->getSerial32();
+				if( !isMember( who ) && !isRecruit( who ) )
+				{
+					recruits[ who ] = new GuildRecruit( pWho );
+					added = true;
+				}
+			}
+			return added;
+		}
+
+		LOGICAL Guild::addMember( P_MEMBER pWho )
+		{
+			return false;
+		}
+
+		typedef Guild* 			P_GUILD;
+		typedef map< SERIAL, P_GUILD >	guildMap;
+		typedef guildMap::iterator	guildMapIterator;
 
 		static	guildMap		guilds;
-		static	guildMemberMap		guildMembers;
 
 		LOGICAL	Create( SERIAL creatorSerial, SERIAL deedSerial )
 		{
-			LOGICAL success = false;
+			LOGICAL created = false;
 			P_CHAR pCreator = pointers::findCharBySerial( creatorSerial );
 
 			if( pCreator )
 			{
 				if( pCreator->IsOnline() )
 				{
-					if( guildMembers.find( creatorSerial ) == guildMembers.end() )
+					guildMapIterator it( guilds.begin() ), end( guilds.end() );
+					LOGICAL isGuildMember = false;
+					while( it != end && !isGuildMember )
+					{
+						isGuildMember = it->second->isMember( creatorSerial ) || it->second->isRecruit( creatorSerial );
+						++it;
+					}
+					if( !isGuildMember )
 					{
 						P_ITEM pDeed = pointers::findItemBySerial( deedSerial );
 						if( pDeed )
@@ -66,7 +282,7 @@ namespace Organisation
 									pStone->magic = 3;
 									pStone->Refresh();
 									pDeed->ReduceAmount(1);
-									success = true;
+									created = true;
 								}
 								else
 								{
@@ -110,18 +326,38 @@ namespace Organisation
 			else
 				LogError("Organisation::Guild::Create - Called using invalid char serial %d\n", creatorSerial );
 
-			return success;
+			return created;
 		}
 
 		LOGICAL	Disband( SERIAL guildSerial, SERIAL adminSerial )
 		{
+			LOGICAL disbanded = false;
+
 			P_CHAR pAdmin = pointers::findCharBySerial( adminSerial );
-			if( pAdmin == 0 )
+			if( pAdmin )
+			{
+				guildMapIterator it( guilds.find( guildSerial ) );
+				if( it != guilds.end() )
+				{
+					if( it->second->isLeader( adminSerial ) )
+					{
+						if( it->second->numberOfMembers() == 1 )
+						{
+							guilds.erase( it );
+							pAdmin->sysmsg(TRANSLATE("Guild has been disbanded."));
+						}
+						else
+						{
+							pAdmin->sysmsg(TRANSLATE("Guild with members cannot be disbanded."));
+						}
+					}
+				}
+			}
+			else
 			{
 				LogError("Organisation::Guild::Disband - Called using invalid char serial %d\n", adminSerial );
-				return false;
 			}
-			return true;
+			return disbanded;
 		}
 
 		LOGICAL EnlistRecruit( SERIAL guildSerial, SERIAL recruiterSerial, SERIAL recruiteeSerial )
