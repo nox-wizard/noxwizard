e68be988bc20dad434ca8e8281aad533f485a036
luxornox <luxornox>
2003-07-20 16:58:20 +0000

    *** empty log message ***

--- range.h
+++ range.h
@@ -12,8 +12,8 @@
 
 #define VERY_VERY_FAR 50000;
 
-R64 dist( Location a, Location b, LOGICAL countZ = !(server_data.disable_z_checking) );
-inline R64 dist( UI32 xa, UI32 ya, SI08 za, UI32 xb, UI32 yb, SI08 zb, LOGICAL countZ = server_data.disable_z_checking )
+R64 dist( Location a, Location b, LOGICAL countZ = (server_data.disable_z_checking == 0) );
+inline R64 dist( UI32 xa, UI32 ya, SI08 za, UI32 xb, UI32 yb, SI08 zb, LOGICAL countZ = (server_data.disable_z_checking == 0) )
 {
 	return dist( Loc( xa, ya, za ), Loc( xb, yb, zb ), countZ );
 }
