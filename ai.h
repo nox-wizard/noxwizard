061fdfeb7adcc8ef93bff492a511097c2726e679
luxornox <luxornox>
2003-07-28 17:09:17 +0000

    *** empty log message ***

--- ai.h
+++ ai.h
@@ -18,6 +18,7 @@
 
 //@{
 /*!
+\author Luxor
 \name Pathfinding method implementation
 */
 #include "nxwcommn.h"
@@ -26,6 +27,15 @@
 #define Z_COST 1
 #define STRAIGHT_COST 10
 #define OBLIQUE_COST 14
+
+/*!
+\author Luxor
+\brief Check flags for isWalkable function
+*/
+enum {
+	WALKFLAG_NONE = 0x0, WALKFLAG_MAP = 0x1, WALKFLAG_STATIC = 0x2, WALKFLAG_DYNAMIC = 0x4, WALKFLAG_CHARS = 0x8, WALKFLAG_ALL = 0xF
+};
+
 
 /*!
 \author Luxor
@@ -39,7 +49,7 @@ struct path_node {
 typedef slist<path_node*> NODE_LIST;
 typedef slist<Location> LOCATION_LIST;
 
-SI08 isWalkable( Location pos );
+SI08 isWalkable( Location pos, UI08 flags = WALKFLAG_ALL );
 
 /*!
 \brief Class implementation of a pathfinding algorithm similar to an A* one
@@ -52,12 +62,11 @@ public:
 	inline Location getFinalPos() { return m_finalPos; }
 	LOGICAL targetReached();
 private:
-	UI32 getLocDist( Location loc1, Location loc2, LOGICAL zEnable = false );
 	path_node* create_node( Location pos, path_node* parentNode, UI32 cost );
         queue<path_node> nodes_vector;
 	UI08 addReachableNodes( path_node* node );
 	void dropToClosedList( path_node* node );
-	void addToOpenList( Location pos, path_node* parentNode, UI32 cost = 10 );
+	void addToOpenList( Location pos, path_node* parentNode, UI32 cost = STRAIGHT_COST );
 	void addToOpenList( path_node* node );
 	void addToClosedList( path_node* node );
         Location m_finalPos;
@@ -68,4 +77,29 @@ private:
 
 //@}
 
+//@{
+/*!
+\author Luxor
+\name Line of sight implementation
+*/
+
+class cRect {
+public:
+	cRect( Location A, Location B );
+	Location getPosAtX( UI32 x );
+	Location getPosAtY( UI32 y );
+	SI08 calcZAtXY( UI32 x, UI32 y );
+private:
+	Location m_posA;
+	SI32 m_xDist;
+	SI32 m_yDist;
+};
+
+namespace los {
+
+LOGICAL lineOfSight( Location pos1, Location pos2 );
+
+}
+
+//@}
 #endif //__AI_H__
