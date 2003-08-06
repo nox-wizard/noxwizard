846d9f7632dbb4654bd05cf4bb3b96ac63845c3a
luxornox <luxornox>
2003-08-06 17:01:53 +0000

    *** empty log message ***

--- ai.h
+++ ai.h
@@ -19,6 +19,67 @@
 //@{
 /*!
 \author Luxor
+\name Fuzzy logic implementation for NPCs
+*/
+
+/*!
+\author Luxor
+\brief Base class of possible actions that would be chosen by an AI
+*/
+class cAction {
+private:
+	cAI *m_ai;
+public:
+	cAction( cAI* ai );
+	virtual void run();
+};
+
+/*!
+\author Luxor
+*/
+class cAttackAction : public cAction {
+private:
+	m_target;
+public:
+	void run();
+	inline P_CHAR getTarget() { return pointers::findCharBySerial( m_target ); }
+};
+
+/*!
+\author Luxor
+*/
+class cDefendAction : public cAction {
+private:
+	m_attacker;
+public:
+	void run();
+	inline P_CHAR getAttacker() { return pointers::findCharBySerial( m_attacker ); }
+};
+
+/*!
+\author Luxor
+\brief Base class of AI implementation
+*/
+class cAI {
+private:
+	SERIAL m_pc;
+	cAction *currAction;
+public:
+	cAI( cChar &pc, SI32 type );
+	virtual void run();
+	cAction* getAction()
+	void setAction();
+	inline LOGICAL hasAction() { return ( currAction != NULL ); }
+	inline P_CHAR getChar() { return pointers::findCharBySerial( m_pc ); }
+};
+
+
+//@}
+
+
+//@{
+/*!
+\author Luxor
 \name Pathfinding method implementation
 */
 #include "nxwcommn.h"
