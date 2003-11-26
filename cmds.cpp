2dc33baa6b0db8110651ab5aa682b45f8f6b27ef
frodosf <frodosf>
2003-11-26 00:07:37 +0000

    Commands System work in progress :)

--- cmds.cpp
+++ cmds.cpp
@@ -1,1 +1,73 @@
+  /*-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+    || NoX-Wizard UO Server Emulator (NXW) [http://noxwizard.sourceforge.net]  ||
+    ||                                                                         ||
+    || This software is free software released under GPL2 license.             ||
+    || You can find detailed license information in nox-wizard.cpp file.       ||
+    ||                                                                         ||
+    || For any question post to NoX-Wizard forums.                             ||
+    -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-*/
+
+
+//file cmds.cpp
+//by Frodo & Stonedz
+
+
+#include "cmds.h"
+
+
+
+//Implementation of cCommand Class
+
+cCommand::cCommand(char* name, long int priv) {
+
+	this->cmd_name=name;
+	this->cmd_priv=priv;
+}
+
+
+
+
+//Implementation of cCommandMap Class
+
+
+P_COMMAND cCommandMap::addGmCommand(char* name, int long priv, void callback()) {
+
+	P_COMMAND cmd= new cCommand(name, priv, callback());
+	P_COMMAND old= command_map[name];
+    command_map[name]= cmd;
+ 	return old;
+}
+
+
+
+P_COMMAND cCommandMap::addGmCommand(P_COMMAND cmd) {
+ 	
+	P_COMMAND old= command_map[cmd->cmd_name];
+    command_map[cmd->cmd_name]= cmd;
+ 	return old;
+}
+
+
+
+P_COMMAND cCommandMap::findCommand(char* name) {
+    return command_map[name];
+}
+
+
+
+td_cmditer cCommandMap::getIteratorBegin() {			// Not sure if necessary 
+    return command_map.begin();
+}
+
+
+td_cmditer cCommandMap::getIteratorEnd() {			// Not sure if necessary
+    return command_map.end();
+}
+
+
+
+
+
+
+
 
--- cmds.h
+++ cmds.h
@@ -1,1 +1,59 @@
+  /*-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+    || NoX-Wizard UO Server Emulator (NXW) [http://noxwizard.sourceforge.net]  ||
+    ||                                                                         ||
+    || This software is free software released under GPL2 license.             ||
+    || You can find detailed license information in nox-wizard.cpp file.       ||
+    ||                                                                         ||
+    || For any question post to NoX-Wizard forums.                             ||
+    -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-*/
 
+
+//file cmds.h
+//by Frodo & Stonedz
+
+
+#include "commons_libs.h"
+#include <bitset>
+
+
+typedef cCommand* P_COMMAND;
+typedef std::map< string, P_COMMAND> td_cmdmap;
+typedef td_cmdmap::iterator td_cmditer;
+
+
+
+//brief Declaration of cCommand Class
+
+
+class cCommand {
+
+public:
+
+	cCommand(char* cmd_name, long int cmd_priv, void callback());
+	
+	void (*cmd_extra) ();
+    char* cmd_name;
+	long int cmd_priv;
+};
+
+
+
+//brief Declaration of cCommandMap Class
+
+
+class cCommandMap {
+
+public:
+	
+	cCommandMap();
+	P_COMMAND addGmCommand(char* name, int long priv, void callback());
+	P_COMMAND addGmCommand(P_COMMAND cmd);
+	P_COMMAND findCommand(char* name);
+	
+	td_cmditer getIteratorBegin();
+	td_cmditer getIteratorEnd();
+
+private:
+
+	td_cmdmap command_map;
+};
