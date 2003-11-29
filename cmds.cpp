aa4bb9b66e6afb4a26dfc6bcb903aaa66c2d70f8
stonedz <stonedz>
2003-11-29 20:32:30 +0000

    some little adds, work in progress, not yet compilable.

--- cmds.cpp
+++ cmds.cpp
@@ -13,13 +13,143 @@
 //Work in progress...
 
 
+
+#include "nxwcommn.h"
+#include "network.h"
 #include "cmds.h"
+#include "sregions.h"
+#include "bounty.h"
+#include "sndpkg.h"
+#include "debug.h"
+#include "amx/amxcback.h"
+#include "calendar.h"
+#include "race.h"
+#include "scp_parser.h"
+#include "commands.h"
+#include "addmenu.h"
+#include "telport.h"
+#include "accounts.h"
+#include "worldmain.h"
+#include "data.h"
+#include "spawn.h"
+#include "trade.h"
+#include "chars.h"
+#include "items.h"
+#include "basics.h"
+#include "inlines.h"
+#include "nox-wizard.h"
+#include "archive.h"
+#include "map.h"
+#include "jail.h"
+#include "skills.h"
+#include "layer.h"
+#include "scripts.h"
+
+
+
+
+/*///////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////
+//Implementations of classes declared in cmds.h
+////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////*/
+
+
+//Implementation of cCommand Class
+
+cCommand::cCommand(std::string name, SI08 number ,AmxFunction* callback) {
+
+	cmd_name=name;
+	cmd_number=number; 
+	cmd_callback=callback;
+}
+
+//Implementation of cCallCommand Class
+
+cCallCommand::cCallCommand (/*...*/){
+
+	/*...*/
+}
+
+cCallCommand::~cCallCommand() {
+
+	
+}
+
+
+//Implementation of cCommandMap Class
+
+cCommandMap::cCommandMap() {
+
+	// all addGmCommand(...); goes here.
+
+}
+
+
+P_COMMAND cCommandMap::addGmCommand(std::string name, SI08 number, AmxFunction* callback) {
+
+	P_COMMAND cmd= new cCommand(name, priv, number,callback);
+    command_map[name]= cmd;
+ 	return cmd;
+}
+
+
+
+/*	Frodo: probably useless (not me, this function :P)
+
+P_COMMAND cCommandMap::addGmCommand(P_COMMAND cmd) {
+ 	
+	P_COMMAND old= command_map[cmd->cmd_name];
+    command_map[cmd->cmd_name]= cmd;
+ 	return old;
+} */
+
+
+P_COMMAD cCommandMap::findCommand(std::string name) {
+	return command_map[name];
+}
+
+/*
+P_COMMAND cCommandMap::findCommand(std::string name) {
+	
+CMDMAP::iterator iter( command_map.find( "name" ) );
+
+if ( iter != command_map.end() )	//command exists
+    return iter->second.callback;
+else
+    return NULL;					//command doesnt exist
+}
+*/
+
+
+cCommandMap* commands = new cCommandMap();
+
+
+
+
+
+
+/*///////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////
+//Todo's
+////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////*/
+
+
+/*
+*******
+A function that controls if the char can do the specified command and
+prepare a cCallCommand object to be given at Small function.
+This function is called after the control in speech.cpp
+*******
+
+*/
 
 
 
 //The function that is called after the control done in speech.cpp
 //This should be put in another file or in a namespace (?)
-
+/*
 void Command(NXWSOCKET  s, char* speech) // Client entred a '/' command like /ADD
 	{
 		unsigned char *comm;
@@ -48,112 +178,17 @@ void Command(NXWSOCKET  s, char* speech) // Client entred a '/' command like /AD
 		client->sysmsg("Unrecognized command: %s", comm);
 			return;
 		}
-		/*
 		
-		  Here there must be a control between cCommand privilege and cChar privilege.
+		
+		  //Here there must be a control between cCommand privilege and cChar privilege.
 
-		  To be continued...
-		*/
+		  //To be continued...
+		
 
 		
 		
 
 	}
-
-
-
-
-/*///////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////////
-//Implementations of classes declared in cmds.h
-////////////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////////*/
-
-
-//Implementation of cCommand Class
-
-cCommand::cCommand(std::string name, SI32 priv,SI08 params_number ,AmxFunction* callback) {
-
-	cmd_name=name;
-	cmd_priv=priv; //stonedz: should be a std::bitset (?)
-	cmd_params_number=params_number;
-	cmd_callback=callback;
-}
-
-
-bool cCommand::isValid(P_CHAR character) {
-	return !notValid(character);
-}
-
-
-
-//Implementation of cCommandMap Class
-
-cCommandMap::cCommandMap() {
-
-	// all addGmCommand(...); goes here.
-
-}
-
-
-P_COMMAND cCommandMap::addGmCommand(std::string name, SI32 priv,SI08 params_number ,AmxFunction* callback) {
-
-	P_COMMAND cmd= new cCommand(name, priv, params_number,callback);
-    command_map[name]= cmd;
- 	return cmd;
-}
-
-
-
-/*	Frodo: probably useless (not me, this function :P)
-
-P_COMMAND cCommandMap::addGmCommand(P_COMMAND cmd) {
- 	
-	P_COMMAND old= command_map[cmd->cmd_name];
-    command_map[cmd->cmd_name]= cmd;
- 	return old;
-} */
-
-
-
-P_COMMAND cCommandMap::findCommand(std::string name) {
-	
-CMDMAP::iterator iter( command_map.find( "name" ) );
-
-if ( iter != command_map.end() )	//command exists
-    return iter->second.callback;
-else
-    return NULL;					//command doesnt exist
-}
-
-
-
-cCommandMap* commands = new cCommandMap();
-
-
-
-
-/*///////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////////
-//Todo's
-////////////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////////*/
-
-
-/*
-
-*******
-A function to construct a cCallCommand object and call the small function
-for the command
-*******
-
-
-void command_execute( NXWCLIENT ps, P_COMMAND cmd , cCallCommand* params )
-{
- ...
-
-}
-
 */
 
 
@@ -162,15 +197,13 @@ void command_execute( NXWCLIENT ps, P_COMMAND cmd , cCallCommand* params )
 
 /*
 
-
 *******
 Must add a native function for AMX to get command property
 for use it in small scripting.
 Something like :
 
 \param 1 char serial
-\param 2 command serial
-\param 3 property
+\param 2 cCallCommand serial
 \param ...  
 
   NATIVE (_getCmdProperty){
@@ -184,7 +217,7 @@ Something like :
 
 /* Frodo:	must add the following function in AMX_NATIVE_INFO nxw_API[] 
 			{ "getCmdProperty", _getCmdProperty }
-*/
+
 
 NATIVE (_getCmdProperty) {		//this is only a copy of getCharProperty, waiting for list of properties
 
@@ -240,3 +273,4 @@ NATIVE (_getCmdProperty) {		//this is only a copy of getCharProperty, waiting fo
   	return INVALID;
 }
 
+*/
--- cmds.h
+++ cmds.h
@@ -12,10 +12,11 @@
 //by Frodo & Stonedz
 //Work in progress...
 
+//#ifndef __CMDS_H
+//#define __CMDS_H
 
-//#include <bitset>
-//#include "common_libs.h"
-#include "nxwcommn.h"		//for std Class
+#include "common_libs.h"
+#include "nxwcommn.h"		//for std classes
 
 class cCommand;
 
@@ -32,14 +33,12 @@ class cCommand {
 
 public:
 
-	cCommand(std::string cmd_name, SI32 cmd_priv, SI08 params_number ,AmxFunction* callback);
+	cCommand(std::string cmd_name, SI08 cmd_number, AmxFunction* callback);
 	
 	std::string cmd_name;
-	SI32 cmd_priv;  //stonedz: this should be a std::bitset (?) //frodo: bitset later defined in cChar
+	SI08 cmd_number;  
 	AmxFunction* cmd_callback;
-	SI08 cmd_params_number ; //Max number of parameters the command should have.
-							 //There is a problem if a command uses more than one 
-							 //multi-word parameter (e.g. 'COMMAND param one param two).
+
 };
 
 
@@ -48,9 +47,9 @@ public:
 //a cCallCommand object is created every time a command is executed. 
 //and destroyed whn the command has finished
 //All parameters given by the char who call the command goes here.
-//stonedz :Maybe, if Small supports it, we can use an array dynamically allocated.
- 
 
+//stonedz :Maybe, if Small supports it, we can use an array dynamically allocated(?).
+ 
 
 class cCallCommand : public cCommand {
 
@@ -58,12 +57,6 @@ public:
 
 	cCallCommand(/*...*/);
 	~cCallCommand();
-
-	string param1;
-	string param2;
-	/* .... */
-	string paramN;
-	//N is the Max number of parameters any command in nox could have
 
 }
  
@@ -77,7 +70,7 @@ class cCommandMap {
 public:
 	
 	cCommandMap();
-	P_COMMAND addGmCommand(std::string name, SI32 priv,SI08 params_number ,AmxFunction* callback);
+	P_COMMAND addGmCommand(std::string name, SI08 number ,AmxFunction* callback);
 	P_COMMAND findCommand(std::string name);
 	
 
