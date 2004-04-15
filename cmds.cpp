71cd9270a8aa1c503cb7ecf3d33afcdf1aae5091
frodosf <frodosf>
2004-04-15 20:58:23 +0000

    new command system features (params)

--- cmds.cpp
+++ cmds.cpp
@@ -176,9 +176,9 @@ void Command(NXWSOCKET  s, char* speech) // Client entered a command like 'ADD
 		unsigned char *comm;
 		unsigned char nonuni[512];
 		char command_line[300];
-		char* params;
-		char* pch;
-		int n=0;
+		//char* params;
+		//char* pch;
+		//int n=0;
 
 		//cmd_offset = 1;
 
@@ -210,18 +210,18 @@ void Command(NXWSOCKET  s, char* speech) // Client entered a command like 'ADD
 		return;	
 		}
 		
-		params=&speech[strlen((char*)comm)+2];
+		/*params=&speech[strlen((char*)comm)+2];
 		strcpy(command_line, params);
 
 		pch = strtok (command_line," ");
-		client->sysmsg("%s!\n", command_line);
+		client->sysmsg("%s!\n", pch);
 		
 		while (pch != NULL){
 			if( strcmp( pch, '\0' )!=0 ){ pc_currchar->setCommandParams(n, pch);
-			/*client->sysmsg("%s!\n", pch);*/}
+			client->sysmsg("%s!\n", pch);}
 			pch = strtok (NULL, " ");
 			n++;
-		}
+		}*/
   
 		
 		p_cmd->call(pc_currchar); //Let's call the Small Function
